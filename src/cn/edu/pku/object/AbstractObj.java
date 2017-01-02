45a4beff632f514464299174b79adb2d9639ce0c
lanzheng <lanzheng@pku.edu.cn>
2017-01-02 23:07:16 +0800

    增加数据导出类

---
+++ src/cn/edu/pku/object/AbstractObj.java
@@ -0,0 +1,534 @@
+package cn.edu.pku.object;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import cn.edu.pku.conf.DatabaseConf;
+import cn.edu.pku.conf.ZhilianConf;
+import cn.edu.pku.util.FileOutput;
+import cn.edu.pku.util.TimeUtil;
+
+public class AbstractObj {
+
+	protected String postitle = new String ();
+	protected String posSalary = new String ();
+	protected String posLocation = new String ();
+	protected String posPublishDate = new String ();
+	protected String posType = new String ();
+	protected String posExperience = new String ();
+	protected String posDegree = new String ();
+	protected String posRecruitNum = new String ();
+	protected String posCategory = new String ();
+	protected String posDescription = new String ();
+	protected String posUrl = new String ();
+	
+	protected String comScale = new String ();
+	protected String comType = new String ();
+	protected String comIndustry = new String ();
+	protected String comHost = new String ();
+	protected String comLocation = new String ();
+	
+	protected int hasTag = 0;	
+	protected String source = new String ();
+	protected String snapshotUrl = new String ();
+	protected String displayContent = new String ();
+	
+	/**
+	 * 将数据导入到文件中
+	 * @param outputPath 导入文件路径
+	 * @param sources 数据来源
+	 * @param data 数据时间范围
+	 * @param industries 行业类别
+	 * @param fields 数据域
+	 * */
+	public static void FeildsToText(String outputPath,
+			String[] sources,
+			String[] date,
+			String[] industries,
+			String[] fields) {
+		
+		String sql = "select ";
+		
+		//fields
+		if (fields == null || fields.length == 0) {
+			System.out.println("info: no fields selected");
+			return;
+		}
+		sql += fields[0];
+		for (int i = 1; i < fields.length; i ++) {
+			sql += ", " + fields[i];
+		}
+		
+		//sources
+		if (sources == null || sources.length == 0) {
+			System.out.println("info: no sources selected");
+			return;
+		}
+		sql += " from "
+				+ DatabaseConf.getPositiontable()
+				+ " where ("
+				+ "source = '"
+				+ sources[0] + "'";
+		for (int i = 1; i < sources.length; i ++) {
+			sql += " or source = '" + sources[i] + "'";
+		}
+		sql += ")";
+		
+		//industries
+		if (industries == null || industries.length == 0) {
+			System.out.println("info: no industries selected");
+			return;
+		}
+		sql += " and (com_industry = '" + industries[0] + "'";
+		for (int i = 1; i < industries.length; i ++) {
+			sql += " or com_industry = '" + industries[i] + "'";
+		}
+		sql += ")";
+		
+		//date
+		if (date == null || date.length == 0) {
+			System.out.println("info: no date specified");
+			return;
+		}
+		sql += " and (pos_publish_date > '" + date[0] + "'";
+		if (date.length == 2) {
+			sql += " and pos_publish_date <= '" + date[1] + "'";
+		}
+		sql += ");";
+		System.out.println(sql);
+		
+		List list = new ArrayList<>();
+		String url = DatabaseConf.getDatebaseurl();
+		try {
+			Class.forName(DatabaseConf.getClassname());
+			Connection conn;
+			try {
+				conn = DriverManager.getConnection(url);
+
+				PreparedStatement stmt;
+				try {
+					stmt = conn.prepareStatement(sql);
+					ResultSet rs = stmt.executeQuery(sql);
+					try {
+						list = convertList(rs);
+					} catch (SQLException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					stmt.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		int counter = 0;
+		FileOutput fo = new FileOutput(outputPath);
+		try {
+			if (fo.t3 != null) {
+				Iterator it = list.iterator();  
+				while(it.hasNext()) {   
+				    Map hm = (Map)it.next();
+				    for (int i = 0; i < fields.length; i ++) {
+				    	fo.t3.write(hm.get(fields[i]).toString() + "	");
+				    }
+				    fo.t3.newLine();
+				    counter ++;
+				}
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		fo.closeOutput();
+		System.out.println("info : " + counter + " pieces of results");
+	}
+	
+	/**
+	 * 将结果保存为list，因为:
+	 * 1.ResultSet可能有最大数量限制；
+	 * 2.需要在遍历的同时删除，以便节省空间;
+	 * */
+	public static List convertList(ResultSet rs) throws SQLException{
+		List list = new ArrayList();
+		ResultSetMetaData md = rs.getMetaData();//获取键名
+		int columnCount = md.getColumnCount();//获取行的数量
+		while (rs.next()) {
+			Map rowData = new HashMap();//声明Map
+			for (int i = 1; i <= columnCount; i ++) {
+				rowData.put(md.getColumnName(i), rs.getObject(i));//获取键名及值
+			}
+			list.add(rowData);
+		}
+		return list;
+	}
+	
+	public AbstractObj () {
+		super();
+	}	
+	
+	public AbstractObj(String postitle, String posSalary, String posLocation, String posPublishDate, String posType,
+			String posExperience, String posDegree, String posRecruitNum, String posCategory, String posDescription,
+			String posUrl, String comScale, String comType, String comIndustry, String comHost, String comLocation,
+			int hasTag, String source, String snapshotUrl, String displayContent) {
+		super();
+		this.postitle = postitle;
+		this.posSalary = posSalary;
+		this.posLocation = posLocation;
+		this.posPublishDate = posPublishDate;
+		this.posType = posType;
+		this.posExperience = posExperience;
+		this.posDegree = posDegree;
+		this.posRecruitNum = posRecruitNum;
+		this.posCategory = posCategory;
+		this.posDescription = posDescription;
+		this.posUrl = posUrl;
+		this.comScale = comScale;
+		this.comType = comType;
+		this.comIndustry = comIndustry;
+		this.comHost = comHost;
+		this.comLocation = comLocation;
+		this.hasTag = hasTag;
+		this.source = source;
+		this.snapshotUrl = snapshotUrl;
+		this.displayContent = displayContent;
+	}
+	
+	public AbstractObj(AbstractObj a) {
+		this.postitle = a.postitle;
+		this.posSalary = a.posSalary;
+		this.posLocation = a.posLocation;
+		this.posPublishDate = a.posPublishDate;
+		this.posType = a.posType;
+		this.posExperience = a.posExperience;
+		this.posDegree = a.posDegree;
+		this.posRecruitNum = a.posRecruitNum;
+		this.posCategory = a.posCategory;
+		this.posDescription = a.posDescription;
+		this.posUrl = a.posUrl;
+		this.comScale = a.comScale;
+		this.comType = a.comType;
+		this.comIndustry = a.comIndustry;
+		this.comHost = a.comHost;
+		this.comLocation = a.comLocation;
+		this.hasTag = a.hasTag;
+		this.source = a.source;
+		this.snapshotUrl = a.snapshotUrl;
+		this.displayContent = a.displayContent;
+	}
+
+	public String getPostitle() {
+		return postitle;
+	}
+	
+	public void setPostitle(String postitle) {
+		this.postitle = postitle;
+	}
+	
+	public String getPosSalary() {
+		return posSalary;
+	}
+	
+	public void setPosSalary(String posSalary) {
+		this.posSalary = posSalary;
+	}
+	
+	public String getPosLocation() {
+		return posLocation;
+	}
+	
+	public void setPosLocation(String posLocation) {
+		this.posLocation = posLocation;
+	}
+	
+	public String getPosPublishDate() {
+		return posPublishDate;
+	}
+	
+	public void setPosPublishDate(String posPublishDate) {
+		this.posPublishDate = posPublishDate;
+	}
+	
+	public String getPosType() {
+		return posType;
+	}
+	
+	public void setPosType(String posType) {
+		this.posType = posType;
+	}
+	
+	public String getPosExperience() {
+		return posExperience;
+	}
+	
+	public void setPosExperience(String posExperience) {
+		this.posExperience = posExperience;
+	}
+	
+	public String getPosDegree() {
+		return posDegree;
+	}
+	
+	public void setPosDegree(String posDegree) {
+		this.posDegree = posDegree;
+	}
+	
+	public String getPosRecruitNum() {
+		return posRecruitNum;
+	}
+	
+	public void setPosRecruitNum(String posRecruitNum) {
+		this.posRecruitNum = posRecruitNum;
+	}
+	
+	public String getPosCategory() {
+		return posCategory;
+	}
+	
+	public void setPosCategory(String posCategory) {
+		this.posCategory = posCategory;
+	}
+	
+	public String getPosDescription() {
+		return posDescription;
+	}
+	
+	public void setPosDescription(String posDescription) {
+		this.posDescription = posDescription;
+	}
+	
+	public String getPosUrl() {
+		return posUrl;
+	}
+	
+	public void setPosUrl(String posUrl) {
+		this.posUrl = posUrl;
+	}
+	
+	public String getComScale() {
+		return comScale;
+	}
+	
+	public void setComScale(String comScale) {
+		this.comScale = comScale;
+	}
+	
+	public String getComType() {
+		return comType;
+	}
+	
+	public void setComType(String comType) {
+		this.comType = comType;
+	}
+	
+	public String getComIndustry() {
+		return comIndustry;
+	}
+	
+	public void setComIndustry(String comIndustry) {
+		this.comIndustry = comIndustry;
+	}
+	
+	public String getComHost() {
+		return comHost;
+	}
+	
+	public void setComHost(String comHost) {
+		this.comHost = comHost;
+	}
+	
+	public String getComLocation() {
+		return comLocation;
+	}
+	
+	public void setComLocation(String comLocation) {
+		this.comLocation = comLocation;
+	}
+	
+	public int getHasTag() {
+		return hasTag;
+	}
+	
+	public void setHasTag(int hasTag) {
+		this.hasTag = hasTag;
+	}
+	
+	public String getSource() {
+		return source;
+	}
+	
+	public void setSource(String source) {
+		this.source = source;
+	}
+	
+	public String getSnapshotUrl() {
+		return snapshotUrl;
+	}
+	
+	public void setSnapshotUrl(String snapshotUrl) {
+		this.snapshotUrl = snapshotUrl;
+	}
+	
+	public String getDisplayContent() {
+		return displayContent;
+	}
+	
+	public void setDisplayContent(String displayContent) {
+		this.displayContent = displayContent;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((comHost == null) ? 0 : comHost.hashCode());
+		result = prime * result + ((comIndustry == null) ? 0 : comIndustry.hashCode());
+		result = prime * result + ((comLocation == null) ? 0 : comLocation.hashCode());
+		result = prime * result + ((comScale == null) ? 0 : comScale.hashCode());
+		result = prime * result + ((comType == null) ? 0 : comType.hashCode());
+		result = prime * result + ((displayContent == null) ? 0 : displayContent.hashCode());
+		result = prime * result + hasTag;
+		result = prime * result + ((posCategory == null) ? 0 : posCategory.hashCode());
+		result = prime * result + ((posDegree == null) ? 0 : posDegree.hashCode());
+		result = prime * result + ((posDescription == null) ? 0 : posDescription.hashCode());
+		result = prime * result + ((posExperience == null) ? 0 : posExperience.hashCode());
+		result = prime * result + ((posLocation == null) ? 0 : posLocation.hashCode());
+		result = prime * result + ((posPublishDate == null) ? 0 : posPublishDate.hashCode());
+		result = prime * result + ((posRecruitNum == null) ? 0 : posRecruitNum.hashCode());
+		result = prime * result + ((posSalary == null) ? 0 : posSalary.hashCode());
+		result = prime * result + ((posType == null) ? 0 : posType.hashCode());
+		result = prime * result + ((posUrl == null) ? 0 : posUrl.hashCode());
+		result = prime * result + ((postitle == null) ? 0 : postitle.hashCode());
+		result = prime * result + ((snapshotUrl == null) ? 0 : snapshotUrl.hashCode());
+		result = prime * result + ((source == null) ? 0 : source.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AbstractObj other = (AbstractObj) obj;
+		if (comHost == null) {
+			if (other.comHost != null)
+				return false;
+		} else if (!comHost.equals(other.comHost))
+			return false;
+		if (comIndustry == null) {
+			if (other.comIndustry != null)
+				return false;
+		} else if (!comIndustry.equals(other.comIndustry))
+			return false;
+		if (comLocation == null) {
+			if (other.comLocation != null)
+				return false;
+		} else if (!comLocation.equals(other.comLocation))
+			return false;
+		if (comScale == null) {
+			if (other.comScale != null)
+				return false;
+		} else if (!comScale.equals(other.comScale))
+			return false;
+		if (comType == null) {
+			if (other.comType != null)
+				return false;
+		} else if (!comType.equals(other.comType))
+			return false;
+		if (displayContent == null) {
+			if (other.displayContent != null)
+				return false;
+		} else if (!displayContent.equals(other.displayContent))
+			return false;
+		if (hasTag != other.hasTag)
+			return false;
+		if (posCategory == null) {
+			if (other.posCategory != null)
+				return false;
+		} else if (!posCategory.equals(other.posCategory))
+			return false;
+		if (posDegree == null) {
+			if (other.posDegree != null)
+				return false;
+		} else if (!posDegree.equals(other.posDegree))
+			return false;
+		if (posDescription == null) {
+			if (other.posDescription != null)
+				return false;
+		} else if (!posDescription.equals(other.posDescription))
+			return false;
+		if (posExperience == null) {
+			if (other.posExperience != null)
+				return false;
+		} else if (!posExperience.equals(other.posExperience))
+			return false;
+		if (posLocation == null) {
+			if (other.posLocation != null)
+				return false;
+		} else if (!posLocation.equals(other.posLocation))
+			return false;
+		if (posPublishDate == null) {
+			if (other.posPublishDate != null)
+				return false;
+		} else if (!posPublishDate.equals(other.posPublishDate))
+			return false;
+		if (posRecruitNum == null) {
+			if (other.posRecruitNum != null)
+				return false;
+		} else if (!posRecruitNum.equals(other.posRecruitNum))
+			return false;
+		if (posSalary == null) {
+			if (other.posSalary != null)
+				return false;
+		} else if (!posSalary.equals(other.posSalary))
+			return false;
+		if (posType == null) {
+			if (other.posType != null)
+				return false;
+		} else if (!posType.equals(other.posType))
+			return false;
+		if (posUrl == null) {
+			if (other.posUrl != null)
+				return false;
+		} else if (!posUrl.equals(other.posUrl))
+			return false;
+		if (postitle == null) {
+			if (other.postitle != null)
+				return false;
+		} else if (!postitle.equals(other.postitle))
+			return false;
+		if (snapshotUrl == null) {
+			if (other.snapshotUrl != null)
+				return false;
+		} else if (!snapshotUrl.equals(other.snapshotUrl))
+			return false;
+		if (source == null) {
+			if (other.source != null)
+				return false;
+		} else if (!source.equals(other.source))
+			return false;
+		return true;
+	}
+	
+}
--- src/cn/edu/pku/object/ZhilianObj.java
+++ src/cn/edu/pku/object/ZhilianObj.java
@@ -18,38 +18,15 @@ import cn.edu.pku.conf.DatabaseConf;
 import cn.edu.pku.conf.ZhilianConf;
 import cn.edu.pku.util.TimeUtil;
 
-public class ZhilianObj {
+public class ZhilianObj extends AbstractObj {
 
-	private String postitle = new String ();
-	private String posSalary = new String ();
-	private String posLocation = new String ();
-	private String posPublishDate = new String ();
-	private String posType = new String ();
-	private String posExperience = new String ();
-	private String posDegree = new String ();
-	private String posRecruitNum = new String ();
-	private String posCategory = new String ();
-	private String posDescription = new String ();
-	private String posUrl = new String ();
-	
-	private String comScale = new String ();
-	private String comType = new String ();
-	private String comIndustry = new String ();
-	private String comHost = new String ();
-	private String comLocation = new String ();
-	
-	private int hasTag = 0;	
-	private String source = new String ();
-	private String snapshotUrl = new String ();
-	private String displayContent = new String ();
-	
 	//pos_url, pos_publish_date + " " + id
 	private static HashMap<String, String> virtualView = new HashMap<String, String>();
 	//pos_url, pos_publish_date + " " + "-1"
 	private static HashMap<String, ZhilianObj> newData = new HashMap<String, ZhilianObj>();
 	
 	public ZhilianObj() {
-		
+		super();
 	}
 	
 	public ZhilianObj(String postitle, String posSalary, String posLocation, String posPublishDate, String posType,
@@ -165,7 +142,7 @@ public class ZhilianObj {
 		    	continue;
 		    }
 		    virtualView.put(key, value);
-		}  
+		}
 	}
 	
 	/**
@@ -545,7 +522,7 @@ public class ZhilianObj {
 	 * 1.ResultSet可能有最大数量限制；
 	 * 2.需要在遍历的同时删除，以便节省空间;
 	 * */
-	private static List convertList(ResultSet rs) throws SQLException{
+	public static List convertList(ResultSet rs) throws SQLException{
 		List list = new ArrayList();
 		ResultSetMetaData md = rs.getMetaData();//获取键名
 		int columnCount = md.getColumnCount();//获取行的数量
@@ -558,7 +535,6 @@ public class ZhilianObj {
 		}
 		return list;
 	}
-	
 	
 	/**
 	 * 删除数据库中的重复数据，仅以URL作为判断标准，时间不同的情况下保留最近的数据
@@ -636,7 +612,6 @@ public class ZhilianObj {
 		}
 	}
 	
-
 	/**
 	 * 打印数据对象
 	 * */
@@ -662,308 +637,5 @@ public class ZhilianObj {
 		System.out.println(this.snapshotUrl);
 		System.out.println();
 	}
-	
-	
-	
-	public String getPosSalary() {
-		return posSalary;
-	}
-	
-	public void setPosSalary(String posSalary) {
-		this.posSalary = posSalary;
-	}
-	
-	public String getPosLocation() {
-		return posLocation;
-	}
-	
-	public void setPosLocation(String posLocation) {
-		this.posLocation = posLocation;
-	}
-	
-	public String getPosPublishDate() {
-		return posPublishDate;
-	}
-	
-	public void setPosPublishDate(String posPublishDate) {
-		this.posPublishDate = posPublishDate;
-	}
-	
-	public String getPosType() {
-		return posType;
-	}
-	
-	public void setPosType(String posType) {
-		this.posType = posType;
-	}
-	
-	public String getPosExperience() {
-		return posExperience;
-	}
-	
-	public void setPosExperience(String posExperience) {
-		this.posExperience = posExperience;
-	}
-	
-	public String getPosDegree() {
-		return posDegree;
-	}
-	
-	public void setPosDegree(String posDegree) {
-		this.posDegree = posDegree;
-	}
-	
-	public String getPosRecruitNum() {
-		return posRecruitNum;
-	}
-	
-	public void setPosRecruitNum(String posRecruitNum) {
-		this.posRecruitNum = posRecruitNum;
-	}
-	
-	public String getPosCategory() {
-		return posCategory;
-	}
-	
-	public void setPosCategory(String posCategory) {
-		this.posCategory = posCategory;
-	}
-	
-	public String getComScale() {
-		return comScale;
-	}
-	
-	public void setComScale(String comScale) {
-		this.comScale = comScale;
-	}
-	
-	public String getComType() {
-		return comType;
-	}
-	
-	public void setComType(String comType) {
-		this.comType = comType;
-	}
-	
-	public String getComIndustry() {
-		return comIndustry;
-	}
-	
-	public void setComIndustry(String comIndustry) {
-		this.comIndustry = comIndustry;
-	}
-	
-	public String getComHost() {
-		return comHost;
-	}
-	
-	public void setComHost(String comHost) {
-		this.comHost = comHost;
-	}
-	
-	public String getComLocation() {
-		return comLocation;
-	}
-	
-	public void setComLocation(String comLocation) {
-		this.comLocation = comLocation;
-	}
 
-	public String getPosDescription() {
-		return posDescription;
-	}
-
-	public void setPosDescription(String posDescription) {
-		this.posDescription = posDescription;
-	}
-
-	public String getPostitle() {
-		return postitle;
-	}
-
-	public void setPostitle(String postitle) {
-		this.postitle = postitle;
-	}
-
-	public String getPosUrl() {
-		return posUrl;
-	}
-
-	public void setPosUrl(String posUrl) {
-		this.posUrl = posUrl;
-	}
-
-	public String getSnapshotUrl() {
-		return snapshotUrl;
-	}
-	
-	public void setSnapshotUrl(String snapshotUrl) {
-		this.snapshotUrl = snapshotUrl;
-	}
-
-	
-	public int getHasTag() {
-		return hasTag;
-	}
-
-	
-	public void setHasTag(int hasTag) {
-		this.hasTag = hasTag;
-	}
-
-	
-	public String getSource() {
-		return source;
-	}
-
-	
-	public void setSource(String source) {
-		this.source = source;
-	}
-
-	
-	public String getDisplayContent() {
-		return displayContent;
-	}
-
-	
-	public void setDisplayContent(String displayContent) {
-		this.displayContent = displayContent;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((comHost == null) ? 0 : comHost.hashCode());
-		result = prime * result + ((comIndustry == null) ? 0 : comIndustry.hashCode());
-		result = prime * result + ((comLocation == null) ? 0 : comLocation.hashCode());
-		result = prime * result + ((comScale == null) ? 0 : comScale.hashCode());
-		result = prime * result + ((comType == null) ? 0 : comType.hashCode());
-		result = prime * result + ((displayContent == null) ? 0 : displayContent.hashCode());
-		result = prime * result + hasTag;
-		result = prime * result + ((posCategory == null) ? 0 : posCategory.hashCode());
-		result = prime * result + ((posDescription == null) ? 0 : posDescription.hashCode());
-		result = prime * result + ((posDegree == null) ? 0 : posDegree.hashCode());
-		result = prime * result + ((posExperience == null) ? 0 : posExperience.hashCode());
-		result = prime * result + ((posLocation == null) ? 0 : posLocation.hashCode());
-		result = prime * result + ((posPublishDate == null) ? 0 : posPublishDate.hashCode());
-		result = prime * result + ((posRecruitNum == null) ? 0 : posRecruitNum.hashCode());
-		result = prime * result + ((posSalary == null) ? 0 : posSalary.hashCode());
-		result = prime * result + ((posType == null) ? 0 : posType.hashCode());
-		result = prime * result + ((posUrl == null) ? 0 : posUrl.hashCode());
-		result = prime * result + ((postitle == null) ? 0 : postitle.hashCode());
-		result = prime * result + ((snapshotUrl == null) ? 0 : snapshotUrl.hashCode());
-		result = prime * result + ((source == null) ? 0 : source.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		ZhilianObj other = (ZhilianObj) obj;
-		if (comHost == null) {
-			if (other.comHost != null)
-				return false;
-		} else if (!comHost.equals(other.comHost))
-			return false;
-		if (comIndustry == null) {
-			if (other.comIndustry != null)
-				return false;
-		} else if (!comIndustry.equals(other.comIndustry))
-			return false;
-		if (comLocation == null) {
-			if (other.comLocation != null)
-				return false;
-		} else if (!comLocation.equals(other.comLocation))
-			return false;
-		if (comScale == null) {
-			if (other.comScale != null)
-				return false;
-		} else if (!comScale.equals(other.comScale))
-			return false;
-		if (comType == null) {
-			if (other.comType != null)
-				return false;
-		} else if (!comType.equals(other.comType))
-			return false;
-		if (displayContent == null) {
-			if (other.displayContent != null)
-				return false;
-		} else if (!displayContent.equals(other.displayContent))
-			return false;
-		if (hasTag != other.hasTag)
-			return false;
-		if (posCategory == null) {
-			if (other.posCategory != null)
-				return false;
-		} else if (!posCategory.equals(other.posCategory))
-			return false;
-		if (posDescription == null) {
-			if (other.posDescription != null)
-				return false;
-		} else if (!posDescription.equals(other.posDescription))
-			return false;
-		if (posDegree == null) {
-			if (other.posDegree != null)
-				return false;
-		} else if (!posDegree.equals(other.posDegree))
-			return false;
-		if (posExperience == null) {
-			if (other.posExperience != null)
-				return false;
-		} else if (!posExperience.equals(other.posExperience))
-			return false;
-		if (posLocation == null) {
-			if (other.posLocation != null)
-				return false;
-		} else if (!posLocation.equals(other.posLocation))
-			return false;
-		if (posPublishDate == null) {
-			if (other.posPublishDate != null)
-				return false;
-		} else if (!posPublishDate.equals(other.posPublishDate))
-			return false;
-		if (posRecruitNum == null) {
-			if (other.posRecruitNum != null)
-				return false;
-		} else if (!posRecruitNum.equals(other.posRecruitNum))
-			return false;
-		if (posSalary == null) {
-			if (other.posSalary != null)
-				return false;
-		} else if (!posSalary.equals(other.posSalary))
-			return false;
-		if (posType == null) {
-			if (other.posType != null)
-				return false;
-		} else if (!posType.equals(other.posType))
-			return false;
-		if (posUrl == null) {
-			if (other.posUrl != null)
-				return false;
-		} else if (!posUrl.equals(other.posUrl))
-			return false;
-		if (postitle == null) {
-			if (other.postitle != null)
-				return false;
-		} else if (!postitle.equals(other.postitle))
-			return false;
-		if (snapshotUrl == null) {
-			if (other.snapshotUrl != null)
-				return false;
-		} else if (!snapshotUrl.equals(other.snapshotUrl))
-			return false;
-		if (source == null) {
-			if (other.source != null)
-				return false;
-		} else if (!source.equals(other.source))
-			return false;
-		return true;
-	}
 }
