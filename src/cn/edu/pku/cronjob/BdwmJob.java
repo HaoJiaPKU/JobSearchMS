b360d2837fa0fbfe58bcdda43edf12ef9c78f01c
lzhpku <lanzheng@pku.edu.cn>
2017-03-14 15:07:58 +0800

    bdwm

--- src/cn/edu/pku/cronjob/BdwmJob.java
+++ src/cn/edu/pku/cronjob/BdwmJob.java
@@ -5,47 +5,46 @@ import javax.annotation.Resource;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import cn.edu.pku.conf.ZhilianConf;
-import cn.edu.pku.crawler.Zhilian;
+import cn.edu.pku.conf.BdwmConf;
+import cn.edu.pku.crawler.Bdwm;
 import cn.edu.pku.service.PositionIndexService;
 import cn.edu.pku.util.TimeUtil;
 
 @Component
-public class ZhilianJob {
+public class BdwmJob {
 	
 	public void executePipeline() {
-		ZhilianConf zc = new ZhilianConf();
-		zc.run();
-		Zhilian zl = new Zhilian();
+		BdwmConf bc = new BdwmConf();
+		Bdwm bdwm = new Bdwm();
 
 		/**
 		//过期n天前的数据，当前n为90，可以不执行这一步，而是设定处理的数据都在过期日期之后
-		System.out.println("info:	" + ZhilianConf.getSource() + "开始 数据过期	"
+		System.out.println("info:	" + BdwmConf.getSource() + "开始 数据过期	"
 				+ TimeUtil.getCurrentTime("yyyy/MM/dd HH:mm:ss"));
-		zl.expirePositionObject();
-		System.out.println("info:	" + ZhilianConf.getSource() + "完成 数据过期	"
+		bdwm.expirePositionObject();
+		System.out.println("info:	" + BdwmConf.getSource() + "完成 数据过期	"
 				+ TimeUtil.getCurrentTime("yyyy/MM/dd HH:mm:ss"));
 		*/
 		
 		//爬取招聘页面保存到本地
-		System.out.println("info:	" + ZhilianConf.getSource() + "开始 数据抓取	"
+		System.out.println("info:	" + BdwmConf.getSource() + "开始 数据抓取	"
 				+ TimeUtil.getCurrentTime("yyyy/MM/dd HH:mm:ss"));
-		zl.crawlPositionPageBatch(zc);
-		System.out.println("info:	" + ZhilianConf.getSource() + "完成 数据抓取	"
+		bdwm.crawlPositionPageBatch(bc);
+		System.out.println("info:	" + BdwmConf.getSource() + "完成 数据抓取	"
 				+ TimeUtil.getCurrentTime("yyyy/MM/dd HH:mm:ss"));
 		
 		//解析本地的招聘页面，保存到服务器的mysql，重复数据不保存
-		System.out.println("info:	" + ZhilianConf.getSource() + "开始 数据保存	"
+		System.out.println("info:	" + BdwmConf.getSource() + "开始 数据保存	"
 				+ TimeUtil.getCurrentTime("yyyy/MM/dd HH:mm:ss"));
-		zl.parsePositionPageBatch(zc);
-		System.out.println("info:	" + ZhilianConf.getSource() + "完成 数据保存	"
+		bdwm.parsePositionPageBatch(bc);
+		System.out.println("info:	" + BdwmConf.getSource() + "完成 数据保存	"
 				+ TimeUtil.getCurrentTime("yyyy/MM/dd HH:mm:ss"));
 		
 		//删除重复数据
-		System.out.println("info:	" + ZhilianConf.getSource() + "开始 删除重复数据	"
+		System.out.println("info:	" + BdwmConf.getSource() + "开始 删除重复数据	"
 				+ TimeUtil.getCurrentTime("yyyy/MM/dd HH:mm:ss"));
-		zl.removeDuplicate();
-		System.out.println("info:	" + ZhilianConf.getSource() + "完成 删除重复数据	"
+		bdwm.removeDuplicate();
+		System.out.println("info:	" + BdwmConf.getSource() + "完成 删除重复数据	"
 				+ TimeUtil.getCurrentTime("yyyy/MM/dd HH:mm:ss"));
 	}
 }
